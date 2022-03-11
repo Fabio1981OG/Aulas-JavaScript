8a8c19493b520ad941b4a0a23785966fb598a721
Fabio1981OG <fabioogoncalves@hotmail.com>
2022-03-11 15:56:47 -0300

    First commit

---
+++ Modelo - Exercicio 01/script.js
@@ -0,0 +1,24 @@
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+/*var hora = data.getHours()
+*/
+var hora = 18
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12){
+    //Bom dia!
+    img.src = 'fotomanhã.png'
+    document.body.style.background = '#2476b2'
+
+}else if (hora >= 12 && hora <= 18){
+    //Boa tarde!
+    img.src = 'fototarde.png'
+    document.body.style.background = '#f7585d'
+
+}else{
+    //Boa noite!
+    img.src = 'fotonoite.png'
+    document.body.style.background = '#20353a'
+    }
+}
